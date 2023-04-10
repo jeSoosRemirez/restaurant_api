a57f405f602e126419f1dbdfd5a2941405b5dce5
jeSoosRemirez <dkhomych0091@gmail.com>
2023-04-10 16:47:03 +0300

    Add admins for receipts and printers

--- restaurant/receipt/admin.py
+++ restaurant/receipt/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Printer, Receipt
 
-# Register your models here.
+
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'api_key', 'receipt_type', 'point_id')
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('id', 'printer_id', 'type', 'status', 'pdf_file')
+    list_filter = ('printer_id', 'type', 'status')
+    readonly_fields = ('pdf_file',)
+
+
+admin.site.register(Printer, PrinterAdmin)
+admin.site.register(Receipt, ReceiptAdmin)
