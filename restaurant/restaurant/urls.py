70eff7238e91fd8d7997a385b96f16c24913daa8
jeSoosRemirez <dkhomych0091@gmail.com>
2023-04-10 16:28:15 +0300

    Add printers, receipts and generate_receipt urls

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from receipt.views import PrinterViewSet, ReceiptViewSet, GenerateReceiptView
+
+router = routers.DefaultRouter()
+router.register(r'printers', PrinterViewSet)
+router.register(r'receipts', ReceiptViewSet)
+
 
 urlpatterns = [
+    # Admin urls
     path('admin/', admin.site.urls),
+
+    # Receipt urls
+    path('', include(router.urls)),
+    path('generate_receipt/', GenerateReceiptView.as_view(), name='generate_receipt')
 ]
